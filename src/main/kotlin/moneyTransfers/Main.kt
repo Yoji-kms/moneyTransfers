d1fb0b18e56a937fabf5cc20d2966c412d4f56b6
Mashnin Egor <y0g@mail.ru>
2020-10-11 11:52:34 +1000

    catch NumberFormatException added

--- src/main/kotlin/moneyTransfers/Main.kt
+++ src/main/kotlin/moneyTransfers/Main.kt
@@ -1,5 +1,6 @@
 package moneyTransfers
 
+import java.lang.NumberFormatException
 import kotlin.math.max
 import kotlin.math.round
 
@@ -7,18 +8,25 @@ import kotlin.math.round
 fun main() {
     val minCommission = 3500.0F
     val commissionPercent = 0.0075F
+    var moneyInput: Float
 
-    print("Enter the sum (rubles): ")
-    val moneyInput = readLine()?.toFloatOrNull()
-
-    if (moneyInput != null) {
-        val moneyInputPennies = moneyInput * 100
-        val commissionPennies = max(minCommission, moneyInputPennies * commissionPercent)
-        val moneyAmountPennies = moneyInputPennies + commissionPennies
-
-        val commission = round(commissionPennies) / 100
-        val moneyAmount = round(moneyAmountPennies) / 100
-        println("Commission: $commission")
-        println("Money amount with commission: $moneyAmount")
+    while (true) {
+        print("Enter the sum (rubles): ")
+        try {
+            moneyInput = readLine()?.toFloat()!!
+            break
+        } catch (e: NumberFormatException) {
+            e.printStackTrace()
+            println("Wrong input!")
+        }
     }
+
+    val moneyInputPennies = moneyInput * 100
+    val commissionPennies = max(minCommission, moneyInputPennies * commissionPercent)
+    val moneyAmountPennies = moneyInputPennies + commissionPennies
+
+    val commission = round(commissionPennies) / 100
+    val moneyAmount = round(moneyAmountPennies) / 100
+    println("Commission: $commission")
+    println("Money amount with commission: $moneyAmount")
 }
